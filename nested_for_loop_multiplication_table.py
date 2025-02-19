9d05197db4cf553146ca59f684539171085930d1
Peejay-T <pjturing_bsit@plmun.edu.ph>
2025-02-19 22:01:51 +0800

    Update nested_for_loop_multiplication_table.py

--- nested_for_loop_multiplication_table.py
+++ nested_for_loop_multiplication_table.py
@@ -1,5 +1,5 @@
 for i in range(1, 6):
     for j in range(1, 6): # indent this loop!
-         product = i * j  # indent this line!
-         print(f"{product:4}", end="") # indent this line! 
+        product = i * j  # indent this line!
+        print(f"{product:4}", end="") # indent this line! 
     print() # indent this line!
